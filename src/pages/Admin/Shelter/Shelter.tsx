16ed48ff7c37a261a51e8be615ab55540ee469bf
ivanreis96 <ivreis96@gmail.com>
2024-04-23 22:16:07 -0300

    Implementação Componente Input

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,9 +1,15 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
+import styles from './Shelter.module.css'
 
 export function Shelter() {
     return (
         <Panel>
-            Formulário de abrigo
+            <form className={styles.container} onSubmit={}>
+                <Input label="Nome" value="" />
+                <Button type="submit">Salvar dados</Button>
+            </form>
         </Panel>
     )
 }
