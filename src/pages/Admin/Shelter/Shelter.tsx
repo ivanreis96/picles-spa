41e3d1ccc3c5a359526e3271c77f21a032cf741d
ivanreis96 <ivreis96@gmail.com>
2024-04-25 19:43:49 -0300

    Adição da persistencia dos dados já preenchidos no formulário e aplicação do skeleton

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -9,6 +9,9 @@ import { useHookFormMask } from "use-mask-input";
 import { toast } from "sonner";
 import { updateShelter } from "../../../services/shelter/updateShelter";
 import { QueryClient, useQueryClient } from "@tanstack/react-query";
+import { useShelter } from "../../../hooks/useShelter";
+import { useEffect } from "react";
+import { Skeleton } from "../../../components/common/skeleton";
 
 const shelterSchema = z.object({
     name: z.string().min(2, 'Nome deve ter no mínimo 2 caracteres.').max(30, 'Nome deve ter no máximo 30 caracteres'),
@@ -27,9 +30,23 @@ const shelterSchema = z.object({
 type ShelterSchema = z.infer<typeof shelterSchema>
 
 export function Shelter() {
-    const { register, handleSubmit, formState } = useForm<ShelterSchema>({
+  
+
+    const { register, handleSubmit, formState, reset } = useForm<ShelterSchema>({
         resolver: zodResolver(shelterSchema)
     })
+
+    const { data, isLoading } = useShelter()
+    useEffect(() => {
+        if(!isLoading && data){
+            reset({
+                name: data.shelterName,
+                email: data.shelterEmail,
+                phone: data.shelterPhone,
+                whatsApp: data.shelterWhatsApp,
+            })
+        }
+    }, [data, isLoading])
 
     const registerWithMask = useHookFormMask(register)
     const queryClient = useQueryClient()
@@ -61,33 +78,41 @@ export function Shelter() {
 
     return (
         <Panel>            
-            <form className={styles.container} onSubmit={handleSubmit(submit)}>
-                <div>
-                    <Input label="Nome" {...register('name')} />
-                    {
-                        formState.errors?.name && (<p className={styles.formError}>{formState.errors.name.message}</p>
-                    )}
-                </div>
-                <div>
-                    <Input label="Email" {...register('email')}/>
-                    {
-                        formState.errors?.email && (<p className={styles.formError}>{formState.errors.email.message}</p>
-                    )}
-                </div>
-                <div>
-                    <Input label="Telefone" {...registerWithMask('phone', ['99 9999-9999', '99 99999-9999'])}/>
-                    {
-                        formState.errors?.phone && (<p className={styles.formError}>{formState.errors.phone.message}</p>
-                    )}
-                </div>
-                <div>
-                    <Input label="WhatsApp" {...registerWithMask('whatsApp', ['99 9999-9999', '99 99999-9999'])}/>
-                    {
-                        formState.errors?.whatsApp && (<p className={styles.formError}>{formState.errors.whatsApp.message}</p>
-                    )}
-                </div>
-                <Button type="submit">Salvar dados</Button>
-            </form>
+            {
+                isLoading && <Skeleton count={4} width={320} height={32} />
+
+            }
+            
+            {!isLoading && (
+                    
+                <form className={styles.container} onSubmit={handleSubmit(submit)}>
+                    <div>
+                        <Input label="Nome" {...register('name')} />
+                        {
+                            formState.errors?.name && (<p className={styles.formError}>{formState.errors.name.message}</p>
+                        )}
+                    </div>
+                    <div>
+                        <Input label="Email" {...register('email')}/>
+                        {
+                            formState.errors?.email && (<p className={styles.formError}>{formState.errors.email.message}</p>
+                        )}
+                    </div>
+                    <div>
+                        <Input label="Telefone" {...registerWithMask('phone', ['99 9999-9999', '99 99999-9999'])}/>
+                        {
+                            formState.errors?.phone && (<p className={styles.formError}>{formState.errors.phone.message}</p>
+                        )}
+                    </div>
+                    <div>
+                        <Input label="WhatsApp" {...registerWithMask('whatsApp', ['99 9999-9999', '99 99999-9999'])}/>
+                        {
+                            formState.errors?.whatsApp && (<p className={styles.formError}>{formState.errors.whatsApp.message}</p>
+                        )}
+                    </div>
+                    <Button type="submit">Salvar dados</Button>
+                </form>
+            )}
         </Panel>
     )
 }
