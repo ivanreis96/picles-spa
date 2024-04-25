442499a6a0d6da9cba69cb03ffb4392f72bc96b7
ivanreis96 <ivreis96@gmail.com>
2024-04-24 22:21:50 -0300

    Validação de formulário com Notificações de erro, salvando, e sucesso

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -6,12 +6,22 @@ import styles from './Shelter.module.css'
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useHookFormMask } from "use-mask-input";
+import { toast } from "sonner";
+import { updateShelter } from "../../../services/shelter/updateShelter";
+import { QueryClient, useQueryClient } from "@tanstack/react-query";
 
 const shelterSchema = z.object({
     name: z.string().min(2, 'Nome deve ter no mínimo 2 caracteres.').max(30, 'Nome deve ter no máximo 30 caracteres'),
     email: z.string().email('Campo deve ser um email.'),
-    phone: z.string(),
-    whatsApp: z.string(), 
+    phone: z.string().refine((value) => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >= 10 &&  digits <= 11
+    }, 'Número deve ter entre 10 e 11 caracteres.'),
+
+    whatsApp: z.string().refine((value) => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >= 10 &&  digits <= 11
+    }, 'Número deve ter entre 10 e 11 caracteres.'),
 })
 
 type ShelterSchema = z.infer<typeof shelterSchema>
@@ -22,13 +32,35 @@ export function Shelter() {
     })
 
     const registerWithMask = useHookFormMask(register)
+    const queryClient = useQueryClient()
 
-    function submit({ name }: ShelterSchema) {
-        console.log(name)
+    async function submit({ name, email, phone, whatsApp }: ShelterSchema) {
+        const toastId = toast.loading('Salvando dados')
+        
+        try {
+            
+            await updateShelter({ 
+                name,
+                email,
+                phone: phone.replace(/\D/g, ''),
+                whatsApp: whatsApp.replace(/\D/g, ''),
+            })
+            queryClient.invalidateQueries({queryKey: ['get-shelter']})
+            toast.success('Dados salvos com sucesso', {
+                id: toastId,
+                closeButton: true,
+            })
+            
+        } catch {
+            toast.error("Não foi possível salvar os dados", {
+                id: toastId,
+                closeButton: true,
+            })
+        }
     }
 
     return (
-        <Panel>
+        <Panel>            
             <form className={styles.container} onSubmit={handleSubmit(submit)}>
                 <div>
                     <Input label="Nome" {...register('name')} />
@@ -43,13 +75,13 @@ export function Shelter() {
                     )}
                 </div>
                 <div>
-                    <Input label="Telefone" {...register('phone')}/>
+                    <Input label="Telefone" {...registerWithMask('phone', ['99 9999-9999', '99 99999-9999'])}/>
                     {
                         formState.errors?.phone && (<p className={styles.formError}>{formState.errors.phone.message}</p>
                     )}
                 </div>
                 <div>
-                    <Input label="WhatsApp" {...register('whatsApp')}/>
+                    <Input label="WhatsApp" {...registerWithMask('whatsApp', ['99 9999-9999', '99 99999-9999'])}/>
                     {
                         formState.errors?.whatsApp && (<p className={styles.formError}>{formState.errors.whatsApp.message}</p>
                     )}
